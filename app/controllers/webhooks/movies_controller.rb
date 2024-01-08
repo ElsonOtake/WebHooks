026b9d16b8b8991f5b2b07d066b1ea04429fab35
Elson Otake <elsonotake@yahoo.com.br>
2024-01-08 13:30:41 -0300

    Create movies controller

---
+++ app/controllers/webhooks/movies_controller.rb
@@ -0,0 +1,3 @@
+class Webhooks::MoviesController < Webhooks::BaseController
+  # curl -X POST http://localhost:3000/webhooks/movies -H 'Content-Type: application/json' -d '{"title": "The Matrix"}'
+end
