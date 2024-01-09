2b723fea7329d2301c6adeca4d6d7f3762e853dc
Elson Otake <elsonotake@yahoo.com.br>
2024-01-09 17:57:15 -0300

    Add enqueued job assertion

--- test/controllers/webhooks/movies_controller_test.rb
+++ test/controllers/webhooks/movies_controller_test.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class Webhooks::MoviesControllerTest < ActionDispatch::IntegrationTest
+  include ActiveJob::TestHelper
+
   def setup
     file_path = Rails.root.join("test", "fixtures", "webhooks", "movie.json")
     @webhook = JSON.parse(File.read(file_path))
@@ -10,6 +12,7 @@ class Webhooks::MoviesControllerTest < ActionDispatch::IntegrationTest
     assert_difference "InboundWebhook.count" do
       post webhooks_movies_url, params: @webhook
     end
+    assert_enqueued_jobs 1
     assert_response :ok
   end
 end
