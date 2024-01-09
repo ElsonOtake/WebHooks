66f89dc78189ed2b825f22a203d6ac330086b2b2
Elson Otake <elsonotake@yahoo.com.br>
2024-01-09 17:40:54 -0300

    Create verify event

--- app/controllers/webhooks/base_controller.rb
+++ app/controllers/webhooks/base_controller.rb
@@ -1,6 +1,8 @@
 class Webhooks::BaseController < ApplicationController
   # Skip CSRF checks on webhooks because they do not originate from the browser
   skip_forgery_protection
+
+  before_action :verify_event
   
   def create
     InboundWebhook.create!(body: payload)
@@ -8,6 +10,10 @@ class Webhooks::BaseController < ApplicationController
   end
 
   private
+
+  def verify_event
+    head :bad_request
+  end
 
   def payload
     @payload ||= request.body.read
--- app/controllers/webhooks/stripe_controller.rb
+++ app/controllers/webhooks/stripe_controller.rb
@@ -5,4 +5,20 @@ class Webhooks::StripeController < Webhooks::BaseController
     Webhooks::StripeJob.perform_later(record)
     head :ok
   end
+
+  private
+
+  def verify_event
+    signature = request.headers['Stripe-Signature']
+    secret = ENV["STRIPE_SIGNING_SECRET"] || Rails.application.credentials.dig(:stripe, :webhook_signing_secret)
+
+    ::Stripe::Webhook::Signature.verify_header(
+      payload,
+      signature,
+      secret.to_s,
+      tolerance: Stripe::Webhook::DEFAULT_TOLERANCE
+    )
+  rescue ::Stripe::SignatureVerificationError
+    head :bad_request
+  end
 end
