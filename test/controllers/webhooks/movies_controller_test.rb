7799fb04e3e8798e27ffb9a041486097f49a0686
Elson Otake <elsonotake@yahoo.com.br>
2024-01-12 09:16:47 -0300

    Add movies controller test

--- test/controllers/webhooks/movies_controller_test.rb
+++ test/controllers/webhooks/movies_controller_test.rb
@@ -15,4 +15,12 @@ class Webhooks::MoviesControllerTest < ActionDispatch::IntegrationTest
     assert_enqueued_jobs 1
     assert_response :ok
   end
+
+  test "should not consume webhook if verification failed" do
+    assert_no_difference "InboundWebhook.count" do
+      post webhooks_movies_url(fail_verification: 1), params: @webhook
+    end
+    assert_enqueued_jobs 0
+    assert_response :bad_request
+  end
 end
